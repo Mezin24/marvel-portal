8fcf73104aa8a42fadbe5f2eb40c57ae22662b69
Pavel Mezencev <mezencev24@mail.ru>
2022-07-18 20:40:30 +0300

    add random char request

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -2,16 +2,39 @@ class MarvelServices {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=70b306b1048a409e977894c03637b4a3';
 
-  getResourse = async () => {
-    const res = await fetch(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
-    );
+  getResourse = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could't fetch url ${url}, status ${res.status}`);
+    }
+
     return await res.json();
   };
 
+  getAllCharacters = async () => {
+    const res = await this.getResourse(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+
+    return await res.data.results[0].map(this._transformCharacters);
+  };
+
   getCharacter = async (id) => {
-    const res = await fetch(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    return await res.json();
+    const res = await this.getResourse(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return await this._transformCharacters(res.data.results[0]);
+  };
+
+  _transformCharacters = (data) => {
+    return {
+      name: data.name,
+      description: data.description,
+      thumbnail: data.thumbnail.path + '.' + data.thumbnail.extension,
+      homepage: data.urls[0].url,
+      wiki: data.urls[1].url,
+    };
   };
 }
 
